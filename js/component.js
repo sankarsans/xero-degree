b1942fccf49fb5bf88eece2b761173fa2f802e90
sankarsans <sssankarsam@gmail.com>
2021-03-06 17:24:06 +0530

    order [age updated

--- js/component.js
+++ js/component.js
@@ -309,8 +309,17 @@ $(window).load(function(){
       })
 })
 
-
-
+function addActive(elem) {
+  // get all 'a' elements
+  var a = document.getElementsByTagName('a');
+  // loop through all 'a' elements
+  for (i = 0; i < a.length; i++) {
+      // Remove the class 'active' if it exists
+      a[i].classList.remove('active')
+  }
+  // add 'active' classs to the element that was clicked
+  elem.classList.add('active');
+}
 
 
 
