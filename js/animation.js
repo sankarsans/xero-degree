460871bd6663283b5a1d2187b20d04e4c45081f5
sankarsans <sssankarsam@gmail.com>
2021-03-13 16:27:38 +0530

    new changes

--- js/animation.js
+++ js/animation.js
@@ -7,12 +7,27 @@ $(function() {
     tl1.addPause();
    
     var time = 0.2;
+    
+    var element = document.querySelector("#new1 h1");
+    var childSplit = new SplitText("h1", {
+        type: "lines, chars",
+        linesClass: "split-child"
+      })
+      var parentSplit = new SplitText("h1", {
+        linesClass: "split-parent"
+      })
 
     tl1.staggerFrom( $(".header_nav ul li"), time, {delay: 0, y:-100, opacity:0, ease : Sine.easeIn }, time );
     tl1.staggerFrom( $(".new-nav ul li", 'swiper-pagination ul li'), time, {delay: 0, x:-30, opacity:0, ease : Sine.easeIn }, time );
-    tl1.from("#new1 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.3");
+    // tl1.from("#new1 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.3");
     tl1.from("#new1 a", 0.2,{y:+10, opacity:0, ease: Elastic});
     tl1.from("#new1 img" ,0.5, {x:-600},"-=1");
+
+    tl1.from(childSplit.lines, 1.7 ,{  yPercent: 100, ease: 'power4.out', stagger: 0.1 })
+        // .to(childSplit.chars, 1.7, { x: 39,ease: 'power4.out',stagger: 0   }, '+=1')
+
+
+
 
 
     var scene = new ScrollMagic.Scene({
@@ -39,7 +54,7 @@ $(function() {
     var controller = new ScrollMagic.Controller({vertical: false});
 
     var tl2 = new TimelineMax();
-   
+
     tl2.from("#new2 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
     tl2.from("#new2 a", 0.2,{y:+10, opacity:0, ease: Elastic});
     tl2.from("#new2 img" ,0.5, {x:-600},"-=1");
@@ -126,11 +141,11 @@ $(function() {
 
     var tl6 = new TimelineMax();
    
-    var time = 0.2;
+    var time = 0.1;
 
-    tl6.staggerFrom( $("#section02 .list-menu ul li"), 0.2, { y:+40, opacity:0, ease : Sine.easeIn }, time );
+    tl6.staggerFrom( $("#section02 .list-menu ul li"), 0.1, { y:+40, opacity:0, ease : Sine.easeIn }, time );
     tl6.from("#section02 h2", 0.2,{y:+30, opacity:0, ease: Sine.easeIn});
-    tl6.staggerFrom( $("#section02 .menu-items .text-center"), 0.1 ,{y:+10, opacity:0, ease: Elastic}, time);
+    tl6.staggerFrom( $("#section02 .menu-items .text-center"), 0 ,{y:+10, opacity:0, ease: Elastic}, time);
     // tl6.from("#section02 img" ,1, {x:-600});
 
 
@@ -520,321 +535,6 @@ $(function() {
     .setTween(tl22)
     .addTo(controller);
 
-
-
-    // // ****************** animation project section ******************//
-    // var controller = new ScrollMagic.Controller({vertical: false});
-
-    // var tl1 = new TimelineMax();
-    // // var time = 0.5;  
-
-    // // tl1.from(".card", 0.4, { scale: 2.5 } );
-    // // tl1.from(".card", 0.4, { y:300} );
-    // // debugger;
-    // // TweenMax.staggerFrom( $(".left-nav ul li"), time, {delay: 4, x:-30, opacity:0, ease : Sine.easeIn }, time );
-
-    // // tl1.from(".header_nav", 0.5,{y:-30, opacity:0, ease: Sine.easeIn},'+=2.5');  
-    // // tl1.from(".left-nav", 0.5,{x:-30, opacity:0, ease: Sine.easeIn},'+=1');
-
-    // var scene = new ScrollMagic.Scene({
-    //     triggerElement:"#section05",
-    //     triggerHook: "onEnter",
-    //     offset: 500,
-    //     duration: 1000,
-    //     reverse: true,
-    // })
-
-    // // .setPin("#project")
-    // .setTween(tl1)
-    // .setClassToggle("#section06", "active")
-    // // .addIndicators()
-    // .addTo(controller);
-
-
-
-
-
-
-
-
-
-
-    // // ****************** animation project section ******************//
-    // var controller = new ScrollMagic.Controller({vertical: false});
-
-    // var tl8 = new TimelineMax();
-    
-
-    // // var cov = $(this).find("#section02 .cover");
-    // // var img = $(this).find("#section02 img");
-
-    // // tl8.from(cov, 1, { scaleX: 0, transformOrigin: "left" });
-    // // tl8.to(cov, 1, { scaleX: 0, transformOrigin: "right" }, "reveal");
-    // // tl8.from(img, 1, { opacity: 0 }, "reveal");
-
-    // // // tl2.from("#section02 img" ,1, {x:-600});
-    // var newtime = 0.4;
-
-    // tl8.from("#section02 h2", 1.0, { y:-100,delay:5,  ease : Sine.easeIn });
-    // tl8.staggerFrom( $("#section02 .list-menu li"), newtime, {delay: 0, y:-100, opacity:0, ease : Sine.easeIn }, newtime );
-    // tl8.staggerFrom( $("#section02 .text-center"), newtime, {delay: 0, y:-100, opacity:0, ease : Sine.easeIn }, newtime );
-
-    // // tl2.from("#section02 h1", 1.0, { y:100,  ease : Sine.easeIn });
-    // // tl2.from("#section02 .description", 1, { y:100, opacity:0,  ease : Sine.easeIn });
-    // // tl2.from("#section02 .card-details", 0.4, { y:200,  ease : Sine.easeIn });
-    
-    // // var time = 0.5;
-    // // tl2.staggerFrom( $("#section02 .card-details"), time, {delay: 2, y:100, opacity:0, ease : Sine.easeIn }, time );
-    
-    // // tl2.to("#section02 img", 0.4, { width:"100%", ease:Power0.easeNone } );
-    // // debugger;
-    // // TweenMax.staggerFrom( $(".left-nav ul li"), time, {delay: 4, x:-30, opacity:0, ease : Sine.easeIn }, time );
-
-    // // tl1.from(".header_nav", 0.5,{y:-30, opacity:0, ease: Sine.easeIn},'+=2.5');  
-    // // tl1.from(".left-nav", 0.5,{x:-30, opacity:0, ease: Sine.easeIn},'+=1');
-
-    // var scene = new ScrollMagic.Scene({
-    //     triggerElement:"#section02",
-    //     triggerHook: 200,
-    //     offset: 0,
-    //     duration: 1000,
-    //     reverse: true,
-    // })
-
-    // // .setPin("#project")
-    // .setTween(tl8)
-    // .setClassToggle("#section02", "active")
-    // // .addIndicators()
-    // .addTo(controller);
-
-
-
-
-
-
-
-
-
-    // // ****************** animation project section ******************//
-    // var controller = new ScrollMagic.Controller({vertical: false});
-
-    // var tl2 = new TimelineMax();
-    
-
-    // var cov = $(this).find("#section03 .cover");
-    // var img = $(this).find("#section03 img");
-
-   
-
-    // // tl2.from("#section02 img" ,1, {x:-600});
-    // var newtime = 0.4;
-
-    // tl2.staggerFrom( $("#section03 .nav li"), newtime, {delay: 0, y:-100, opacity:0, ease : Sine.easeIn }, newtime );
-    // tl2.from("#section03 h3", 1.0, { y:-100, opacity:0, ease : Sine.easeIn },'+=8');
-    // tl2.from("#section03 h1", 1.0, { y:100,opacity:0,  ease : Sine.easeIn });
-    // tl2.from("#section03 .description", 1, { y:100, opacity:0,  ease : Sine.easeIn });
-    // // tl2.from("#section02 .card-details", 0.4, { y:200,  ease : Sine.easeIn });
-
-    // tl2.from(cov, 1, { delay:2, scaleX: 0, transformOrigin: "left" });
-    // tl2.to(cov, 1, { scaleX: 0, transformOrigin: "right" }, "reveal");
-    // tl2.from(img, 1, { opacity: 0 }, "reveal");
-
-    // var time = 0.5;
-    // tl2.staggerFrom( $("#section03 .card-details"), time, {delay: 2, y:100, opacity:0, ease : Sine.easeIn }, time );
-    // // tl2.from("#section03 .title", 1.0, {css:{position:'fixed'}}, { y:100, opacity:0,  ease : Sine.easeIn });
-
-    // // tl2.to("#section02 img", 0.4, { width:"100%", ease:Power0.easeNone } );
-    // // debugger;
-    // // TweenMax.staggerFrom( $(".left-nav ul li"), time, {delay: 4, x:-30, opacity:0, ease : Sine.easeIn }, time );
-
-    // // tl1.from(".header_nav", 0.5,{y:-30, opacity:0, ease: Sine.easeIn},'+=2.5');  
-    // // tl1.from(".left-nav", 0.5,{x:-30, opacity:0, ease: Sine.easeIn},'+=1');
-
-    // var scene = new ScrollMagic.Scene({
-    //     triggerElement:"#section03",
-    //     pin: true,
-    //     triggerHook: "onEnter",
-    //     offset: 350,
-    //     duration: 1000,
-    //     reverse: true,
-    // })
-
-    // // .setPin("#project")
-    // .setTween(tl2)
-    // .setClassToggle("#section03", "active")
-    // // .addIndicators()
-    // .addTo(controller);
-
-
-
-
-
-
-
-
-
-
-    
-
-
-    // var controller = new ScrollMagic.Controller({vertical: false});
-
-    // var tl4 = new TimelineMax();
-    
-
-    // var cov = $(this).find("#section05 .cover");
-    // var img = $(this).find("#section05 img");
-
-    // tl4.from(cov, 1, { delay:2, scaleX: 0, transformOrigin: "left" });
-    // tl4.to(cov, 1, { scaleX: 0, transformOrigin: "right" }, "reveal");
-    // tl4.from(img, 1, { opacity: 0 }, "reveal");
-
-    // // tl2.from("#section02 img" ,1, {x:-600});
-    // var newtime = 0.4;
-
-    // tl4.staggerFrom( $("#section05 .nav li"), newtime, {delay: 0, y:-100, opacity:0, ease : Sine.easeIn }, newtime );
-    // tl4.from("#section05 h1", 1.0, { y:100, opacity:0, ease : Sine.easeIn },'+=8');
-    // tl4.from("#section05 p", 1, { y:100, opacity:0,  ease : Sine.easeIn });
-    // tl4.from("#section05 .btn", 1.0, { y:100, opacity:0, ease : Sine.easeIn });
-
-    // // tl2.from("#section02 .card-details", 0.4, { y:200,  ease : Sine.easeIn });
-    
-    // var time = 0.5;
-    // tl4.staggerFrom( $("#section05 .card-details"), time, {delay: 2, y:100, opacity:0, ease : Sine.easeIn }, time );
-    
-    // // tl2.to("#section02 img", 0.4, { width:"100%", ease:Power0.easeNone } );
-    // // debugger;
-    // // TweenMax.staggerFrom( $(".left-nav ul li"), time, {delay: 4, x:-30, opacity:0, ease : Sine.easeIn }, time );
-
-    // // tl1.from(".header_nav", 0.5,{y:-30, opacity:0, ease: Sine.easeIn},'+=2.5');  
-    // // tl1.from(".left-nav", 0.5,{x:-30, opacity:0, ease: Sine.easeIn},'+=1');
-
-    // var scene = new ScrollMagic.Scene({
-    //     triggerElement:"#section05",
-    //     triggerHook: "onEnter",
-    //     offset: 350,
-    //     duration: 1000,
-    //     reverse: true,
-    // })
-
-    // // .setPin("#project")
-    // .setTween(tl4)
-    // .setClassToggle("#section05", "active")
-    // // .addIndicators()
-    // .addTo(controller);
-
-
-
-
-    // var controller = new ScrollMagic.Controller({vertical: false});
-
-    // var tl5 = new TimelineMax();
-    
-    // var newtime = 0.4;
-
-    // tl5.staggerFrom( $("#section06 .career-perks"), newtime, {delay: 0, y:-100, opacity:0, ease : Sine.easeIn }, newtime );
-    // tl5.from("#section06 h2", 1.0, { y:100, opacity:0,  ease : Sine.easeIn });
-   
-    // var scene = new ScrollMagic.Scene({
-    //     triggerElement:"#section06",
-    //     triggerHook: "onEnter",
-    //     offset: 350,
-    //     duration: 1000,
-    //     reverse: true,
-    // })
-
-    // // .setPin("#project")
-    // .setTween(tl5)
-    // .setClassToggle("#section06", "active")
-    // // .addIndicators()
-    // .addTo(controller);
-
-
-
-
-    // var controller = new ScrollMagic.Controller({vertical: false});
-
-    // var tl6 = new TimelineMax();
-    
-    // var newtime = 0.4;
-
-    // tl6.staggerFrom( $("#section07 img"), newtime, {delay: 3, opacity:0, ease : Sine.easeIn }, newtime );
-    // tl6.from("#section07 h2", 1.0, { y:100, opacity:0,  ease : Sine.easeIn });
-    // tl6.from("#section07 h4", 1.0, { y:100, opacity:0,  ease : Sine.easeIn });
-
-
-    // var scene = new ScrollMagic.Scene({
-    //     triggerElement:"#section07",
-    //     triggerHook: "onEnter",
-    //     offset: 350,
-    //     duration: 1000,
-    //     reverse: true,
-    // })
-
-    // // .setPin("#project")
-    // .setTween(tl6)
-    // .setClassToggle("#section07", "active")
-    // // .addIndicators()
-    // .addTo(controller);
-
-
-
-    // var controller = new ScrollMagic.Controller({vertical: false});
-
-    // var tl7 = new TimelineMax();
-    
-    // // var newtime = 0.4;
-
-    // // tl7.staggerFrom( $("#section07 img"), newtime, {delay: 3, y:-100, opacity:0, ease : Sine.easeIn }, newtime );
-    // tl7.from("#section04 .list-content", 1.0, { y:100, opacity:0,  ease : Sine.easeIn });
-    // tl7.from("#section04 .col-md-8", 1.0, { y:100, opacity:0,  ease : Sine.easeIn });
-
-
-    // var scene = new ScrollMagic.Scene({
-    //     triggerElement:"#section04",
-    //     triggerHook: "onEnter",
-    //     offset: 350,
-    //     duration: 1000,
-    //     reverse: true,
-    // })
-
-    // // .setPin("#project")
-    // .setTween(tl7)
-    // .setClassToggle("#section04", "active")
-    // // .addIndicators()
-    // .addTo(controller);
-
-
-
-
-
-
-    
-
-
-
-    // var controller = new ScrollMagic.Controller({vertical: false});
-    // var tl93 = new TimelineMax();
-    
-    // // var newtime = 0.4;
-
-    // // tl7.staggerFrom( $("#section07 img"), newtime, {delay: 3, y:-100, opacity:0, ease : Sine.easeIn }, newtime );
-    // // tl93.from(".title", 1.0, {css:{position:'fixed'}}, { y:100, opacity:0,  ease : Sine.easeIn });
-
-    
-    //     var scene = new ScrollMagic.Scene({
-    //         triggerElement:"#section02",
-    //         triggerHook: "onEnter",
-    //         offset: 0,
-    //         duration: 000,
-    //         reverse: true,
-    //     })
-    
-    //     // .setPin("#project")
-    //     .setTween(tl93)
-    //     // .setClassToggle("#contact", "active")
-    //     // .addIndicators()
-    //     .addTo(controller);
 
 
 });
--- js/new.js
+++ js/new.js
@@ -1,54 +1,41 @@
-function PageTransition() {
-    var tl = gsap.timeline();
-    // alert("first")
-    // tl.to(".loading-screen", {duration:1.2, width:"100%", left: "0%", ease:"Expo.easeInOut"})
-    // tl.to(".loading-screen", {duration:1, width:"100%", left:"100%", ease:"Expo.easeInOut", delay:0.3})
-    // tl.set(".loading-screen", {left:"-100%"})
-    // alert("second")
-    tl.to('ul.transition li' , {duration: 1.5, scaleY:1, transformOrigin: "bottom left", stagger: .1 }, "-=1.1")
-    tl.to('ul.transition li' , {duration: .5, scaleY:0, transformOrigin: "bottom left", stagger: .1 , delay: .1})
-}
-
-function contentAnimation() {
-    var tl = gsap.timeline();
-    // tl.to(".loading-screen", {duration:1.2, width:"100%", left: "0%", ease:"Expo.easeInOut"})
-    // tl.to(".loading-screen", {duration:1, width:"100%", left:"100%", ease:"Expo.easeInOut", delay:0.3})
-    // tl.set(".loading-screen", {left:"-100%"})
-    tl.from('.left' , {duration: 1.5, translateY:50, opacity:0 })
-    tl.to('img' , {clipPath:"polygon(0 0 , 100% 0, 100% 100%, 0% 100%)"}, "-=1.1")
-    tl.from('.box' , {duration: .5, scaleX:0, transformOrigin: "bottom left", stagger: .1 , delay: .1 })
-}
-
-
-function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-
-        }, n);
-    });
-}
-
-
-
-barba.init({
-    sync:true,
-
-    transitions: [{
-        async leave(data) {
-            const done = this.async();
-            PageTransition();
-            await delay(1500);
-            done();
-        },
-
-        async enter(data) {
-            contentAnimation();
-        },
-        async once(data) {
-            contentAnimation();
-        }
-    }]
-});
-
+// window.addEventListener("load", function () {
+//     let revealText = document.querySelectorAll("#viewport h1");
+//     let results = Splitting({ target: revealText, by: "lines" });
+  
+//     results.forEach((splitResult) => {
+//       const wrappedLines = splitResult.lines
+//         .map(
+//           (wordsArr) => `
+//           <span class="line"><div class="words">
+//             ${wordsArr
+//               .map(
+//                 (word) => `${word.outerHTML}<span class="whitespace"> 
+//            </span>`
+//               )
+//               .join("")}
+//           </div></span>`
+//         )
+//         .join("");
+//       splitResult.el.innerHTML = wrappedLines;
+//     });
+  
+//     gsap.registerPlugin(ScrollTrigger);
+//     let revealLines = revealText.forEach((element) => {
+//         alert("hello")
+//       const lines = element.querySelectorAll(".line .words");
+  
+//       let tl = gsap.timeline({
+//         scrollTrigger: {
+//           trigger: element,
+//             toggleActions:"restart none none reset",
+//         }
+//       });
+//       tl.set(revealText, { autoAlpha: 1 });
+//       tl.from(lines, 1, {
+//         yPercent: 100,
+//         ease: Power3.out,
+//         stagger: 0.25,
+//         delay: 0.2
+//       });
+//     });
+//   });
