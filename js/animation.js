cf52ff6a2f1e9c23e131957fb384edc91b00fb1e
sankarsans <sssankarsam@gmail.com>
2021-03-14 12:47:28 +0530

    reveal animation changed

--- js/animation.js
+++ js/animation.js
@@ -8,39 +8,36 @@ $(function() {
    
     var time = 0.2;
     
-    // let revealText = document.querySelectorAll("#new1 h1");
-    // let results = Splitting({ target: revealText, by: "lines" });
+    let revealText = document.querySelectorAll("#new1 h1, #new2 h3, #new3 h3, #new4 h3, #new5 h3, #new6 h1, #new6 p.description, #new7 h1, #new7 p, #new7 h3, #new8 p, #new8 h3,#new10 h1, #new10 p, #new11 h3, #new11 h6, #new121 h1, #new121 p,  #new122 h1, #new122 p, #new123 h1, #new123 p,#new13 h2,#new14 h1, #new14 p, #new15 h2,  #new16 h2, #new16 h4");
+    let results = Splitting({ target: revealText, by: "lines" });
 
-    // results.forEach((splitResult) => {
-    //     const wrappedLines = splitResult.lines
-    //     .map(
-    //         (wordsArr) => `
-    //         <span class="line"><div class="words">
-    //         ${wordsArr
-    //             .map(
-    //             (word) => `${word.outerHTML}<span class="whitespace"> 
-    //         </span>`
-    //             )
-    //             .join("")}
-    //         </div></span>`
-    //     )
-    //     .join("");
-    //     splitResult.el.innerHTML = wrappedLines;
-    // });
+    results.forEach((splitResult) => {
+        const wrappedLines = splitResult.lines
+        .map(
+            (wordsArr) => `
+            <span class="line"><div class="words">
+            ${wordsArr
+                .map(
+                (word) => `${word.outerHTML}<span class="whitespace"> 
+            </span>`
+                )
+                .join("")}
+            </div></span>`
+        )
+        .join("");
+        splitResult.el.innerHTML = wrappedLines;
+    });
 
-    // gsap.registerPlugin(ScrollTrigger);
-    // let revealLines = revealText.forEach((element) => {
-    //     alert("hell")
-    //     const lines = element.querySelectorAll(".line .words");
-    //     let tl11 = gsap.timeline({scrollTrigger: {trigger: "#new1",start: "top top",toggleActions:"restart none none reset",}});
-
-    //     tl11.set(revealText, { autoAlpha: 1 });
-    //     tl11.from(lines, 1, {yPercent: 100,ease: Power3.out,stagger: 0.25,delay: 0.2});
-    // });
+    gsap.registerPlugin(ScrollTrigger);
+    let revealLines = revealText.forEach((element) => {
+        const lines = element.querySelectorAll(".line .words");
+        let tl11 = gsap.timeline({scrollTrigger: {trigger: "#new1",start: "top top",toggleActions:"restart none none reset",}});
+        tl11.set(revealText, { autoAlpha: 1 });
+    });
 
     tl1.staggerFrom( $(".header_nav ul li"), time, {delay: 0, y:-100, opacity:0, ease : Sine.easeIn }, time );
     tl1.staggerFrom( $(".new-nav ul li", 'swiper-pagination ul li'), time, {delay: 0, x:-30, opacity:0, ease : Sine.easeIn }, time );
-    tl1.from("#new1 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.3");
+    tl1.staggerFrom( $("#new1 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.3");
     tl1.from("#new1 a", 0.2,{y:+10, opacity:0, ease: Elastic});
     tl1.from("#new1 img" ,0.5, {x:-600},"-=1");
 
@@ -81,9 +78,38 @@ $(function() {
     //   var parentSplit2 = new  SplitText("h3", {
     //     linesClass: "split-parent"
     //   })
+
+    // let revealText1 = document.querySelectorAll("#new2 h3");
+    // let results1 = Splitting({ target: revealText, by: "lines1" });
+
+    // results1.forEach((splitResult) => {
+    //     alert("hello")
+    //     const wrappedLines1 = splitResult.lines1
+    //     .map(
+    //         (wordsArr) => `
+    //         <span class="line"><div class="words">
+    //         ${wordsArr
+    //             .map(
+    //             (word) => `${word.outerHTML}<span class="whitespace"> 
+    //         </span>`
+    //             )
+    //             .join("")}
+    //         </div></span>`
+    //     )
+    //     .join("");
+    //     splitResult.el.innerHTML = wrappedLines1;
+    // });
+
+    // gsap.registerPlugin(ScrollTrigger);
+    // let revealLines1 = revealText1.forEach((element) => {
+    //     const lines1 = element.querySelectorAll(".line .words");
+    //     let tl12 = gsap.timeline({scrollTrigger: {trigger: "#new2",start: "top top",toggleActions:"restart none none reset",}});
+    //     tl12.set(revealText1, { autoAlpha: 1 });
+    // });
       
     //   tl2.from(childSplit2.lines, 1.7 ,{  yPercent: 100, ease: 'power4.out', stagger: 0.1 })
-    tl2.from("#new2 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+    tl2.staggerFrom( $("#new2 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+    // tl2.from("#new2 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
     tl2.from("#new2 a", 0.2,{y:+10, opacity:0, ease: Elastic});
     tl2.from("#new2 img" ,0.5, {x:-600},"-=1");
 
@@ -104,7 +130,8 @@ $(function() {
 
     var tl3 = new TimelineMax();
    
-    tl3.from("#new3 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+    tl3.staggerFrom( $("#new3 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+    // tl3.from("#new3 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
     tl3.from("#new3 a", 0.2,{y:+10, opacity:0, ease: Elastic});
     tl3.from("#new3 img" ,0.5, {x:-600},"-=1");
 
@@ -126,7 +153,9 @@ $(function() {
 
     var tl4 = new TimelineMax();
    
-    tl4.from("#new4 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+    tl4.staggerFrom( $("#new4 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+
+    // tl4.from("#new4 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
     tl4.from("#new4 a", 0.2,{y:+10, opacity:0, ease: Elastic});
     tl4.from("#new4 img" ,0.5, {x:-600},"-=1");
 
@@ -147,7 +176,9 @@ $(function() {
 
     var tl5 = new TimelineMax();
    
-    tl5.from("#new5 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+    tl5.staggerFrom( $("#new5 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+
+    // tl5.from("#new5 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
     tl5.from("#new5 a", 0.2,{y:+10, opacity:0, ease: Elastic});
     tl5.from("#new5 img" ,0.5, {x:-600},"-=1");
 
@@ -195,8 +226,13 @@ $(function() {
     var tl7 = new TimelineMax();
    
     tl7.from("#new6 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-    tl7.from("#new6 h1", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
-    tl7.from("#new6 p", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
+    // tl7.staggerFrom( $("#new6 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+    // tl7.staggerFrom( $("#new6 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+    tl7.staggerFrom( $("#new6 h1 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+    tl7.staggerFrom( $("#new6 p .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    // tl7.from("#new6 h1", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
+    // tl7.from("#new6 p", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
     tl7.from("#new6 .card-details", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
     tl7.from("#new6 img" ,0.5, {x:-600},"-=1");
 
@@ -218,8 +254,11 @@ $(function() {
     var tl8 = new TimelineMax();
    
     tl8.from("#new7 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-    tl8.from("#new7 h1", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
-    tl8.from("#new7 p", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
+    tl8.staggerFrom( $("#new7 h1 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+    tl8.staggerFrom( $("#new7 p .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    // tl8.from("#new7 h1", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
+    // tl8.from("#new7 p", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
     tl8.from("#new7 img" ,0.5, {x:-600},"-=1");
 
 
@@ -240,9 +279,12 @@ $(function() {
     var tl9 = new TimelineMax();
     var time = 0.2;
 
-    tl9.from("#new8 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-    tl9.from("#new8 p", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
-    tl9.staggerFrom( $("#new8 img"), 0.2, {x:-600}, time);
+    // tl9.from("#new8 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+    tl9.staggerFrom( $("#new8 h3 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+    tl9.staggerFrom( $("#new8 p .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    // tl9.from("#new8 p", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
+    tl9.staggerFrom( $("#new8 img"), 0.2, {x:-600}, "-=1");
 
     var scene = new ScrollMagic.Scene({
         triggerElement:"#new8",
@@ -306,9 +348,13 @@ $(function() {
     var tl12 = new TimelineMax();
     var time = 0.2;
 
+
     tl12.from("#new10 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-    tl12.from("#new10 h1", 0.2,{y:+10, opacity:0, ease: Back});
-    tl12.from("#new10 p", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
+    tl12.staggerFrom( $("#new10 h1 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+    tl12.staggerFrom( $("#new10 p .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    // tl12.from("#new10 h1", 0.2,{y:+10, opacity:0, ease: Back});
+    // tl12.from("#new10 p", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
     tl12.from("#new10 a", 0.2,{y:+10, opacity:0, ease: Sine.easeIn});
     tl12.from("#new10 img" ,0.5, {x:-600},"-=1");
 
@@ -330,8 +376,11 @@ $(function() {
     var tl13 = new TimelineMax();
     var time = 0.2;
 
-    tl13.from("#new11 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-    tl13.from("#new11 h6", 0.2,{y:+10, opacity:0, ease: Back});
+    // tl13.from("#new11 h3", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+    tl13.staggerFrom( $("#new11 h3 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+    tl13.staggerFrom( $("#new11 h6 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    // tl13.from("#new11 h6", 0.2,{y:+10, opacity:0, ease: Back});
     tl13.staggerFrom( $("#new11 .process-wrapper"), 0.2, {x:-100, opacity:0, ease: Elastic }, time);
     tl13.staggerFrom( $("#new11 .connection-arrow"), 0.2, {y:+100, opacity:0, ease: Elastic }, time);
 
@@ -355,8 +404,11 @@ $(function() {
      var tl14 = new TimelineMax();
      var time = 0.2;
  
-     tl14.from("#new121 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-     tl14.from("#new121 p", 0.2,{y:+10, opacity:0, ease: Back});
+    //  tl14.from("#new121 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+     tl14.staggerFrom( $("#new121 h1 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+     tl14.staggerFrom( $("#new121 p .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    //  tl14.from("#new121 p", 0.2,{y:+10, opacity:0, ease: Back});
      tl14.from("#new121 img" ,0.5, {x:-600},"-=1");
  
      var scene = new ScrollMagic.Scene({
@@ -376,8 +428,11 @@ $(function() {
      var tl15 = new TimelineMax();
      var time = 0.2;
  
-     tl15.from("#new122 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-     tl15.from("#new122 p", 0.2,{y:+10, opacity:0, ease: Back});
+     tl15.staggerFrom( $("#new122 h1 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+     tl15.staggerFrom( $("#new122 p .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    //  tl15.from("#new122 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+    //  tl15.from("#new122 p", 0.2,{y:+10, opacity:0, ease: Back});
      tl15.from("#new122 img" ,0.5, {x:-600},"-=1");
  
      var scene = new ScrollMagic.Scene({
@@ -398,8 +453,11 @@ $(function() {
      var tl16 = new TimelineMax();
      var time = 0.2;
  
-     tl16.from("#new123 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-     tl16.from("#new123 p", 0.2,{y:+10, opacity:0, ease: Back});
+    //  tl16.from("#new123 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+     tl16.staggerFrom( $("#new123 h1 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+     tl16.staggerFrom( $("#new123 p .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    //  tl16.from("#new123 p", 0.2,{y:+10, opacity:0, ease: Back});
      tl16.from("#new123 img" ,0.5, {x:-600},"-=1");
  
      var scene = new ScrollMagic.Scene({
@@ -422,7 +480,9 @@ $(function() {
      var time = 0.2;
  
      tl17.from("#new13 h6", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-     tl17.from("#new13 h2", 0.2,{y:+10, opacity:0, ease: Back});
+     tl17.staggerFrom( $("#new13 h2 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    //  tl17.from("#new13 h2", 0.2,{y:+10, opacity:0, ease: Back});
      tl17.staggerFrom( $("#new13 .accordian-wrpper"), 0.2, {y:+30, opacity:0, ease: Elastic }, time);
 
      var scene = new ScrollMagic.Scene({
@@ -443,8 +503,11 @@ $(function() {
      var tl18 = new TimelineMax();
      var time = 0.2;
  
-     tl18.from("#new14 h6", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
-     tl18.from("#new14 h2", 0.2,{y:+10, opacity:0, ease: Back});
+    //  tl18.from("#new14 h1", 0.2,{y:+30, opacity:0, ease: Sine.easeIn},"+=0.4");
+     tl18.staggerFrom( $("#new14 h1 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+     tl18.staggerFrom( $("#new14 p .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    //  tl18.from("#new14 p", 0.2,{y:+10, opacity:0, ease: Back});
      tl18.from("#new14 a", 0.2,{y:+10, opacity:0, ease: Back});
      tl18.from("#new14 img" ,0.5, {x:-600},"-=1");
 
@@ -467,7 +530,9 @@ $(function() {
       var tl19 = new TimelineMax();
       var time = 0.2;
   
-      tl19.from("#new15 h2", 0.2,{y:+30, opacity:0, ease: Back},"+=0.4");
+    //   tl19.from("#new15 h2", 0.2,{y:+30, opacity:0, ease: Back},"+=0.4");
+      tl19.staggerFrom( $("#new15 h2 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+
       tl19.staggerFrom( $("#new15 .career-perks"), 0.2, {y:+30, opacity:0, ease: Elastic }, time);
  
       var scene = new ScrollMagic.Scene({
@@ -489,8 +554,11 @@ $(function() {
        var tl20 = new TimelineMax();
        var time = 0.2;
    
-       tl20.from("#new16 h2", 0.2,{y:+30, opacity:0, ease: Back},"+=0.4");
-       tl20.from("#new16 h4", 0.2,{y:+30, opacity:0, ease: Back});
+    //    tl20.from("#new16 h2", 0.2,{y:+30, opacity:0, ease: Back},"+=0.4");
+       tl20.staggerFrom( $("#new16 h2 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut },"+=0.4");
+       tl20.staggerFrom( $("#new16 h4 .words"), 0.9,{y:+30, delay: 0.2, opacity:0, ease: Power3.easeOut });
+
+    //    tl20.from("#new16 h4", 0.2,{y:+30, opacity:0, ease: Back});
        tl20.staggerFrom( $("#new16 img"), 0.2, {y:+30, opacity:0, ease: Elastic }, time);
   
        var scene = new ScrollMagic.Scene({
